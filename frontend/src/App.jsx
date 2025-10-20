83833e905aff80ffc57129343731e45054dfc2c1
23001497-lang <23001497@hus.edu.vn>
2025-10-20 17:54:47 +0700

    Update App.jsx
    update App

---
+++ frontend/src/App.jsx
@@ -0,0 +1,28 @@
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Login from "./Components/login";
+import Dashboard from "./Components/Dashboard";
+
+import Home from "./Components/Home";
+import Employee from "./Components/Employee";
+import Category from "./Components/Category";
+import Profile from "./Components/Profile";
+import { BrowserRouter, Routes, Route} from "react-router-dom";
+function App() {
+  return (
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Login />}></Route>
+      <Route path="/login" element={<Login />}></Route>
+      <Route path="/dashboard" element={<Dashboard />}>
+        <Route path='' element={<Home />}></Route>
+        <Route path='/dashboard/employee' element={<Employee />}></Route>
+        <Route path='/dashboard/category' element={<Category />}></Route>
+        <Route path='/dashboard/profile' element={<Profile/>}></Route>
+      </Route>
+    </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
