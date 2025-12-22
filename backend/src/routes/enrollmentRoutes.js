32df755c021fd18427554fd71ab7f533fad1f3d7
quynh2196 <2196quynhdodoan@gmail.com>
2025-12-23 02:37:08 +0700

    mini update

--- backend/src/routes/enrollmentRoutes.js
+++ backend/src/routes/enrollmentRoutes.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import { enroll, withdraw } from '../controllers/enrollmentController.js';
-import authMiddleware from '../middleware/auth.js';
+import authMiddleware from '../middleware/initRedisSession.js';
 
 const router = express.Router();
 
