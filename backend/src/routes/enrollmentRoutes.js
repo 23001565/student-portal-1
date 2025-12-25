7526afe4f288ca716dbc072b97d3c5e30fc571b7
quynhf21 <2196quynhdodoan@gmail.com>
2025-12-26 02:02:14 +0700

    update

--- backend/src/routes/enrollmentRoutes.js
+++ backend/src/routes/enrollmentRoutes.js
@@ -1,19 +1,11 @@
 import express from 'express';
 import { enroll, withdraw } from '../controllers/enrollmentController.js';
-import authMiddleware from '../middleware/initRedisSession.js';
+import initRedisSession from '../middleware/initRedisSession.js';
 
 const router = express.Router();
 
-/**
- * Sinh viên đăng ký lớp
- * POST /enrollments
- */
 router.post('/', authMiddleware, enroll);
 
-/**
- * Sinh viên hủy đăng ký lớp
- * DELETE /enrollments/:classId
- */
 router.delete('/:classId', authMiddleware, withdraw);
 
 export default router;
