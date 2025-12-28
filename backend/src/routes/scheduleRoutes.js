7c7b717fd3a77a6d8e7299b059c3263a89b144ac
quynhf21 <2196quynhdodoan@gmail.com>
2025-12-28 16:36:03 +0700

    Update scheduleRoutes.js

--- backend/src/routes/scheduleRoutes.js
+++ backend/src/routes/scheduleRoutes.js
@@ -1,1 +1,10 @@
 
+import express from 'express';
+import { mySchedule } from '../controllers/scheduleController.js';
+import initRedisSession from '../middleware/initRedisSession.js';
+
+const router = express.Router();
+
+router.get('/me/schedule', initRedisSession, mySchedule);
+
+export default router;
