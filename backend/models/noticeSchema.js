91bca6de85221af99905f9ac314a3dad0bf70a7b
quynhf21 <2196quynhdodoan@gmail.com>
2025-12-31 00:15:35 +0700

    update

--- backend/models/noticeSchema.js
+++ backend/models/noticeSchema.js
@@ -1,23 +1,23 @@
-const mongoose = require("mongoose")
+// const mongoose = require("mongoose")
 
-const noticeSchema = new mongoose.Schema({
-    title: {
-        type: String,
-        required: true
-    },
-    details: {
-        type: String,
-        required: true
-    },
-    date: {
-        type: Date,
-        required: true
-    },
-    school: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'admin'
-    },
-}, { timestamps: true });
+// const noticeSchema = new mongoose.Schema({
+//     title: {
+//         type: String,
+//         required: true
+//     },
+//     details: {
+//         type: String,
+//         required: true
+//     },
+//     date: {
+//         type: Date,
+//         required: true
+//     },
+//     school: {
+//         type: mongoose.Schema.Types.ObjectId,
+//         ref: 'admin'
+//     },
+// }, { timestamps: true });
 
-module.exports = mongoose.model("notice", noticeSchema)
+// module.exports = mongoose.model("notice", noticeSchema)
 
