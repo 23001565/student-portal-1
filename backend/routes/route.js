91cef87ac55cdba0c2919ea0bd46d4a022faa7d0
quynhf21 <2196quynhdodoan@gmail.com>
2025-12-31 00:14:46 +0700

    update

--- backend/routes/route.js
+++ backend/routes/route.js
@@ -1,20 +1,20 @@
-import express from 'express';
-import {
-  profile,
-  updateProfile,
-  myEnrollments,
-  mySchedule
-} from '../controllers/studentController.js';
-import initRedisSession from '../middleware/initRedisSession.js';
+// import express from 'express';
+// import {
+//   profile,
+//   updateProfile,
+//   myEnrollments,
+//   mySchedule
+// } from '../controllers/studentController.js';
+// import initRedisSession from '../middleware/initRedisSession.js';
 
-const router = express.Router();
+// const router = express.Router();
 
-// Thông tin cá nhân
-router.get('/me', initRedisSession, profile);
-router.put('/me', initRedisSession, updateProfile);
+// // Thông tin cá nhân
+// router.get('/me', initRedisSession, profile);
+// router.put('/me', initRedisSession, updateProfile);
 
-// Đăng ký & lịch học
-router.get('/me/enrollments', initRedisSession, myEnrollments);
-router.get('/me/schedule', initRedisSession, mySchedule);
+// // Đăng ký & lịch học
+// router.get('/me/enrollments', initRedisSession, myEnrollments);
+// router.get('/me/schedule', initRedisSession, mySchedule);
 
-export default router;
+// export default router;
