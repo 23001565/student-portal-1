840b644568ab55119896140c7baba87bbc649cc8
quynhf21 <2196quynhdodoan@gmail.com>
2025-12-31 00:13:49 +0700

    update

--- backend/controllers/notice-controller.js
+++ backend/controllers/notice-controller.js
@@ -1,20 +1,20 @@
 
-import { getStudentSchedule } from '../services/scheduleService.js';
+// import { getStudentSchedule } from '../services/scheduleService.js';
 
-export async function mySchedule(req, res) {
-  try {
-    const studentId = req.user.id;
-    const { semester, year } = req.query;
+// export async function mySchedule(req, res) {
+//   try {
+//     const studentId = req.user.id;
+//     const { semester, year } = req.query;
 
-    const schedule = await getStudentSchedule(
-      studentId,
-      Number(semester),
-      Number(year)
-    );
+//     const schedule = await getStudentSchedule(
+//       studentId,
+//       Number(semester),
+//       Number(year)
+//     );
 
-    res.json(schedule);
-  } catch (err) {
-    console.error('Get schedule error:', err);
-    res.status(500).json({ error: 'Failed to fetch schedule' });
-  }
-}
+//     res.json(schedule);
+//   } catch (err) {
+//     console.error('Get schedule error:', err);
+//     res.status(500).json({ error: 'Failed to fetch schedule' });
+//   }
+// }
